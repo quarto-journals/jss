c31f440f35c965c8040645b88d1dcb2952c70cd2
Charles Teague <cteague@gmail.com>
2022-06-23 09:59:44 -0400

    Improve author wrapping and output

--- _extensions/quarto-journals/jss/filter.lua
+++ _extensions/quarto-journals/jss/filter.lua
@@ -1,6 +1,7 @@
 
 
-
+-- these classes, when placed on a span will be replaced
+ -- with an identical LaTeX command for PDF output
 local texMappings = {
   "proglang",
   "pkg",
@@ -16,13 +17,30 @@ return {
 
       for i, mapping in ipairs(texMappings) do
         if #el.attr.classes == 1 and el.attr.classes:includes(mapping) then
-            if quarto.doc.isFormat("pdf") then
-                return pandoc.RawInline("tex", "\\" .. mapping .. "{" .. contentStr .. "}" )
-            else 
-                return pandoc.Code(contentStr);
-            end
+          if quarto.doc.isFormat("pdf") then
+            return pandoc.RawInline("tex", "\\" .. mapping .. "{" .. contentStr .. "}" )
+          else 
+            return pandoc.Code(contentStr);
+          end
         end
       end
+    end,
+    Meta = function(meta) 
+      -- Authors output in the template uses a special separator
+      -- to join authors (including wrapping to a new line)
+      -- this computes the proper prefix and places it in the author metadata
+      -- for use by the template
+      local byAuthor = meta['by-author']
+      for i, author in ipairs(byAuthor) do
+        local prefix = {pandoc.RawInline("tex ","")};
+        if i > 1 and i % 2 == 1 then
+          prefix = {pandoc.RawInline("tex", "\\AND")}
+        elseif i > 1 then
+          prefix = {pandoc.RawInline("tex", "\\And")}
+        end
+        author['metadata']['latex-prefix'] = prefix
+      end
+      return meta
     end
   }
 }
